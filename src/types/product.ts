3aab2b2f42671430656084f4cb8a5bc30863a7a5
AliArefnia <maarefnia1377@gmail.com>
2025-05-21 01:26:25 +0330

    add product type

---
+++ src/types/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  productId: string;
+  name: string;
+  sku: string;
+  price: number;
+  quantity: number;
+  category?: string;
+  created_at?: string;
+  updated_at?: string;
+};
