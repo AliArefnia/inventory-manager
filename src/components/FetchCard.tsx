3c0a5928727e3f4d8e5e0cd9606726ca3c635dec
AliArefnia <maarefnia1377@gmail.com>
2025-05-22 21:07:08 +0330

    Create FetchCard.tsx
    which is a card to have fetch data logic

---
+++ src/components/FetchCard.tsx
@@ -0,0 +1,47 @@
+import LoadingSpinner from "./LoadingSpinner";
+
+function FetchCard({
+  title,
+  value,
+  isLoading,
+  isError,
+  error,
+  onRetry,
+}: {
+  title: string;
+  value: React.ReactNode;
+  isLoading: boolean;
+  isError: boolean;
+  error: Error | null;
+  onRetry?: () => void;
+}) {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow">
+      <h3 className="text-lg font-sans">{title}</h3>
+      {isLoading ? (
+        <LoadingSpinner color="red" />
+      ) : isError ? (
+        <div className="text-red-500 text-sm">
+          Error loading.
+          <p>{error?.message}</p>
+          <button
+            onClick={(event) => {
+              if (onRetry) {
+                onRetry();
+              }
+
+              event.stopPropagation();
+            }}
+            className="underline text-blue-600"
+          >
+            Retry
+          </button>
+        </div>
+      ) : (
+        <p className="text-3xl font-bold">{value}</p>
+      )}
+    </div>
+  );
+}
+
+export default FetchCard;
