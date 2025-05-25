5d1156ca3f9f9919fbfdfd03d65117de2f4ffb9b
AliArefnia <maarefnia1377@gmail.com>
2025-05-25 23:46:49 +0330

    Update baseButton.tsx

--- src/components/baseButton.tsx
+++ src/components/baseButton.tsx
@@ -2,7 +2,7 @@ type BaseButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
   disabled?: boolean;
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "borderBlue" | "borderRed";
   className?: string;
 };
 
@@ -10,21 +10,23 @@ export default function BaseButton({
   children,
   onClick,
   disabled = false,
-  variant = "primary",
+  variant,
   className = "",
 }: BaseButtonProps) {
   const baseStyle =
-    "px-4 py-2 rounded transition-colors duration-200 font-medium";
+    "px-4 py-2 rounded transition-colors duration-200 font-medium cursor-pointer  ";
   const variants = {
     primary: `bg-blue-600 text-white hover:bg-blue-700 disabled:bg-blue-300`,
     secondary: `bg-gray-200 text-gray-800 hover:bg-gray-300 disabled:bg-gray-100`,
+    borderBlue: `bg-white rounded border-2 border-blue-600 text-blue-600`,
+    borderRed: `bg-white rounded border-2 border-red-600 text-red-600`,
   };
 
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`${baseStyle} ${variants[variant]} ${className}`}
+      className={`${baseStyle} ${variant && variants[variant]} ${className}`}
     >
       {children}
     </button>
