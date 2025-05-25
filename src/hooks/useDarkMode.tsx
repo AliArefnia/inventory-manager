0a86a58ac2463388d27abef9a7bd51679fee7313
AliArefnia <maarefnia1377@gmail.com>
2025-05-25 23:46:11 +0330

    add useDarkMode and add it to the root

---
+++ src/hooks/useDarkMode.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function useDarkMode() {
+  const [isDark, setIsDark] = useState(
+    () =>
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (isDark) {
+      root.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      root.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  }, [isDark]);
+
+  return [isDark, setIsDark] as const;
+}
