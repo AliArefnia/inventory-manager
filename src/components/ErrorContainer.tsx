ba5e2a17db4c24e60c82bbf7d84ae9420b83c9f5
AliArefnia <maarefnia1377@gmail.com>
2025-06-02 23:04:06 +0330

    change error container style and add classes

--- src/components/ErrorContainer.tsx
+++ src/components/ErrorContainer.tsx
@@ -1,10 +1,13 @@
 type ErrorProps = {
   children: string;
+  className?: string;
 };
 
-export default function ErrorContainer({ children }: ErrorProps) {
+export default function ErrorContainer({ children, className }: ErrorProps) {
   return (
-    <div className="p-4 bg-red-100 text-red-700 rounded-md text-sm mt-4 border border-red-300">
+    <div
+      className={`p-4 bg-red-100 text-red-700 rounded-md text-sm mt-4 border border-red-300 text-center ${className}`}
+    >
       <strong>Error:</strong> {children}
     </div>
   );
