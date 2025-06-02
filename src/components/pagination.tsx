3b35fd4ff58388eae7ae98c4dc4c85a4d695394c
AliArefnia <maarefnia1377@gmail.com>
2025-06-02 23:04:55 +0330

    change pagination style and adapt for dark mode

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -13,8 +13,9 @@ export default function Pagination({
   totalItems,
   changePage,
 }: PaginationProps) {
+  const isLastPage = currentPage === Math.ceil(totalItems! / numberOfItems);
   return (
-    <div className="flex items-center justify-between mt-6 text-sm text-gray-600">
+    <div className="flex items-center justify-between mt-6 text-sm text-gray-500 dark:text-white">
       <span>
         Showing {(currentPage - 1) * numberOfItems + 1}–
         {currentPage * numberOfItems < totalItems!
@@ -23,22 +24,38 @@ export default function Pagination({
         of {totalItems}
       </span>
 
-      <div className="flex items-center gap-2">
+      <div className="flex items-center justify-center gap-4">
         <BaseButton
           onClick={() => changePage(currentPage - 1)}
           disabled={currentPage === 1}
+          className={`border text-sm ${
+            currentPage === 1
+              ? "dark:!bg-neutral-700 dark:text-neutral-400 border-transparent !bg-neutral-600 text-neutral-300 !cursor-not-allowed "
+              : "bg-gray-100 text-gray-400 cursor-pointer border-gray-200"
+          }`}
           variant="secondary"
         >
-          Previous
+          ← Previous
         </BaseButton>
-        <span>
-          Page {currentPage} of {Math.ceil(totalItems! / numberOfItems)}
+
+        <span className="text-sm font-medium text-gray-600 dark:text-gray-300">
+          Page <span className="font-semibold">{currentPage}</span> of{" "}
+          <span className="font-semibold">
+            {Math.ceil(totalItems! / numberOfItems)}
+          </span>
         </span>
+
         <BaseButton
           onClick={() => changePage(currentPage + 1)}
-          disabled={currentPage === Math.ceil(totalItems! / numberOfItems)}
+          disabled={isLastPage}
+          className={`border text-sm ${
+            isLastPage
+              ? "dark:!bg-neutral-700 dark:text-neutral-400 border-transparent !bg-neutral-600 text-neutral-300 !cursor-not-allowed "
+              : "bg-gray-100 text-gray-400 cursor-pointer border-gray-200"
+          }`}
+          variant="secondary"
         >
-          Next
+          Next →
         </BaseButton>
       </div>
     </div>
