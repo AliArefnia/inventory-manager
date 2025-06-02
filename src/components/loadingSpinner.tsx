c135c6a31660c858e45c329395f7c96a42a5f603
AliArefnia <maarefnia1377@gmail.com>
2025-06-02 23:03:19 +0330

    change default color and change some styles

--- src/components/loadingSpinner.tsx
+++ src/components/loadingSpinner.tsx
@@ -6,16 +6,20 @@ type SpinnerProps = {
 
 export default function LoadingSpinner({
   size = 40,
-  color = "blue",
+  color = "black",
   children,
 }: SpinnerProps) {
   return (
     <div className="flex flex-col justify-center">
       <div
-        className="animate-spin rounded-2xl border-4  border-t-transparent self-center"
+        className="animate-spin rounded-2xl border-4 my-2 border-t-transparent self-center"
         style={{ width: size, height: size, borderColor: color }}
       />
-      {children && <p style={{ color: color }}>{children}</p>}
+      {children && (
+        <p style={{ color: color }} className="mt-4 text-center">
+          {children}
+        </p>
+      )}
     </div>
   );
 }
