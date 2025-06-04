7f575058f2ea0a4a137b43d3b8b45ea990590dcd
AliArefnia <maarefnia1377@gmail.com>
2025-06-04 18:19:38 +0330

    modify dashboard
    add new product page to the dashboard
    only role or login can be shown add the same time
    sidebar is open by default
    modify some styles

--- src/pages/DashboardMain.tsx
+++ src/pages/DashboardMain.tsx
@@ -2,26 +2,31 @@ import { Outlet, NavLink } from "react-router-dom";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import useDarkMode from "../hooks/useDarkMode";
+import BaseButton from "../components/BaseButton";
 
 function Dashboard() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
 
   const navItems = [
     { to: "/", label: "Dashboard" },
     { to: "/products", label: "Products" },
     { to: "/categories", label: "Categories" },
+    { to: "/newProduct", label: "New Product" },
   ];
   const [isDark, setIsDark] = useDarkMode();
+
+  const isLoggedIn = true;
+
   return (
-    <div className="min-h-screen flex bg-gray-50 relative overflow-hidden dark:bg-black ">
+    <div className="h-screen flex bg-gray-50 relative overflow-hidden dark:bg-black ">
       {/* Sidebar */}
       <aside
-        className={`fixed z-40  top-0 left-0 md:min-h-fit h-screen w-64 bg-white shadow-md transform transition-transform duration-300 ease-in-out border-r-4 rounded-r-xl border-black/75 
+        className={`fixed z-40 top-0 left-0 md:min-h-fit h-screen w-64 bg-white shadow-md transform transition-transform duration-300 ease-in-out border-r-4 rounded-r-xl border-black/75 dark:border-white/75 
         ${sidebarOpen ? "translate-x-0 md:relative " : "-translate-x-full "} 
          flex-shrink-0 dark:bg-dark-secondary-elevated`}
       >
         <div className="p-6">
-          <h1 className="text-2xl font-bold text-gray-800 mb-8 text-center">
+          <h1 className="text-2xl font-bold text-gray-800 mb-8 text-center uppercase dark:text-gray-200">
             Inventory App
           </h1>
           <nav className="space-y-2">
@@ -35,10 +40,10 @@ function Dashboard() {
                   }
                 }}
                 className={({ isActive }) =>
-                  `block px-4 py-2 rounded-lg transition-colors ${
+                  `block px-4 py-2 text-center rounded-lg transition-colors  ${
                     isActive
-                      ? "bg-gray-200 text-gray-900 font-semibold"
-                      : "text-gray-600 hover:bg-gray-100"
+                      ? "bg-gray-200 font-semibold dark:text-black"
+                      : "text-gray-600 hover:bg-gray-100  dark:text-gray-200 dark:hover:text-gray-800"
                   }`
                 }
               >
@@ -61,27 +66,42 @@ function Dashboard() {
       <div className="flex-1 flex flex-col min-w-0 dark:bg-dark-main">
         <header className="bg-white shadow-sm p-4 flex justify-between items-center dark:bg-dark-secondary-elevated ">
           <div className="flex items-center gap-2">
-            <button
+            <BaseButton
               className="text-gray-700"
               onClick={() => setSidebarOpen((open) => !open)}
             >
-              {sidebarOpen ? <X size={24} /> : <Menu size={24} />}
-            </button>
+              {sidebarOpen ? (
+                <X
+                  size={24}
+                  className="dark:text-white hover:scale-110 transition-all"
+                />
+              ) : (
+                <Menu
+                  size={24}
+                  className="dark:text-white hover:scale-110 transition-all"
+                />
+              )}
+            </BaseButton>
             <h2 className="text-xl text-gray-800 dark:text-gray-200">
               Dashboard
             </h2>
           </div>
           <div className="flex items-center gap-4">
-            <button className="text-sm text-gray-600 hover:text-gray-800">
-              Login
-            </button>
-            <button
+            {isLoggedIn ? (
+              <span className="text-sm font-medium text-gray-700 dark:text-neutral-200">
+                Admin
+              </span>
+            ) : (
+              <BaseButton className="text-sm text-neutral-700 hover:text-black hover:scale-110 dark:text-neutral-200 dark:hover:text-neutral-400">
+                Login
+              </BaseButton>
+            )}
+            <BaseButton
               onClick={() => setIsDark(!isDark)}
-              className="text-sm px-3 py-1 border rounded dark:border-gray-700"
+              className="px-3 py-1 border rounded dark:border-neutral-500 dark:text-neutral-300 hover:scale-105"
             >
               {isDark ? "☀ Light Mode" : "🌙 Dark Mode"}
-            </button>
-            <span className="text-sm font-medium text-gray-700">Admin</span>
+            </BaseButton>
           </div>
         </header>
 
