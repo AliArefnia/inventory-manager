b2c1e5e54a85d8b902bdbc4ee499769c1c1d8285
AliArefnia <maarefnia1377@gmail.com>
2025-06-06 16:20:43 +0330

    add hook to get low products (also based on search)

--- src/hooks/useLowProduct.ts
+++ src/hooks/useLowProduct.ts
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../lib/supabase";
+import { type Product } from "../types/product";
+
+import { useDebounce } from "../hooks/useDebounce";
 import { NUMBER_PER_PAGE, LOW_STOCK_NUMBER } from "../const";
+
 export const useLowStockProductCount = () => {
   return useQuery({
     queryKey: ["low-stock-count"],
@@ -11,6 +15,46 @@ export const useLowStockProductCount = () => {
         .lte("quantity", LOW_STOCK_NUMBER);
       if (error) throw new Error(error.message);
       return count ?? 0;
+    },
+  });
+};
+
+export const usePaginatedLowStockProducts = (
+  page: number,
+  searchTerm: string
+) => {
+  const debouncedSearch = useDebounce(searchTerm);
+
+  return useQuery({
+    queryKey: ["low-stock-products", page, debouncedSearch],
+    queryFn: async () => {
+      const from = (page - 1) * NUMBER_PER_PAGE;
+      const to = from + NUMBER_PER_PAGE - 1;
+
+      let query = supabase
+        .from("products")
+        .select("*", { count: "exact" })
+        .lte("quantity", LOW_STOCK_NUMBER)
+        .range(from, to)
+        .order("quantity", { ascending: true });
+
+      if (debouncedSearch.length >= 2) {
+        query = query.or(
+          `name.ilike.%${debouncedSearch}%,sku.ilike.%${debouncedSearch}%`
+        );
+      }
+
+      const { data, error, count } = await query;
+
+      if (error) throw new Error(error.message);
+
+      return {
+        data: data.map(({ id, ...product }) => ({
+          ...product,
+          productId: id,
+        })) as Product[],
+        count,
+      };
     },
   });
 };
