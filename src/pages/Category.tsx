c5a087a3bccd90c5066dbbc92fb414a03d09b13a
AliArefnia <maarefnia1377@gmail.com>
2025-06-10 17:42:54 +0330

    Update Category.tsx
    update

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -6,7 +6,7 @@ import { usePaginatedProductCategory } from "../hooks/useProducts";
 import ProductTable from "../components/ProductTable";
 import Pagination from "../components/Pagination";
 
-import { NUMBER_PER_PAGE } from "../conts";
+import { NUMBER_PER_PAGE } from "../const";
 
 function Category() {
   const { categoryName } = useParams();
@@ -27,7 +27,7 @@ function Category() {
         placeholder="Search..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="mb-4 px-4 py-2 border rounded"
+        className="w-1/2 px-4 py-2 mx-2 mt-3 mb-6 rounded-lg border border-neutral-300 dark:border-neutral-700 bg-white dark:bg-neutral-900 text-neutral-800 dark:text-neutral-200 placeholder-neutral-400 dark:placeholder-neutral-500 shadow-sm focus:outline-none focus:ring-2 focus:ring-neutral-400 dark:focus:ring-neutral-600 transition"
       />
       <ProductTable
         key={categoryName}
@@ -37,7 +37,7 @@ function Category() {
         isError={isError}
         error={error}
       ></ProductTable>
-      {!isLoading && !isError && (
+      {!isLoading && !isError && count !== 0 && (
         <Pagination
           currentPage={page}
           numberOfItems={NUMBER_PER_PAGE}
