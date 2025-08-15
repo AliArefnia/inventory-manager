1b58bae45051702def2ee844bf44cb21eb23d4e5
AliArefnia <maarefnia1377@gmail.com>
2025-08-15 15:00:48 +0330

    Update main.tsx
    add suspense
    change react query options

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
 import "./assets/css/tailwind.css";
 
-import { StrictMode } from "react";
+import React, { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+      retry: 1,
+      staleTime: 1000 * 60,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+});
+
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import Login from "./pages/Login";
+const Login = React.lazy(() => import("./pages/Login"));
 import Register from "./pages/Register";
 import DashboardMain from "./pages/DashboardMain";
 import ProductsAll from "./pages/ProductsAll";
@@ -28,8 +40,16 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DashboardMain />}>
-            <Route path="/dashboard" index element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/dashboard" element={<Home />} />
+            <Route
+              path="/login"
+              element={
+                <Suspense fallback={<p>Loading...</p>}>
+                  <Login />
+                </Suspense>
+              }
+            />
             <Route path="/register" element={<Register />} />
             <Route path="products" element={<ProductsAll />} />
             <Route path="newProduct" element={<ProductNew />} />
@@ -38,6 +58,7 @@ createRoot(document.getElementById("root")!).render(
             <Route path="categories" element={<Categories />} />
             <Route path="categories/:categoryName" element={<Category />} />
           </Route>
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <ToastContainer position="top-right" />
       </BrowserRouter>
